ade7164bc42212e4a4795354c09fb13fa76a59fa
Developer1 <developer.one@example.com>
2025-12-05 21:02:32 +0100

    links in the text of the template index.html has been changed from *.html to the URI like

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def landing():
     return render_template("index.html")
 
+@app.route("/weightform")
+def weightform():
+    return render_template("weightform.html")
+
+@app.route("/showall")
+def showall():
+    return render_template("showall.html")
+
+@app.route("/store")
+def store():
+    return render_template("store.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
